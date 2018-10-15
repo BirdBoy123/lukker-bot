be80f8eea019685ceb96db533d01cca649066e2b
troyunculuk <44125716+troyunculuk@users.noreply.github.com>
2018-10-15 15:52:14 +0300

    Add files via upload

---
+++ komutlar/yardımet.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+
+
+exports.run = function(client, message) {
+
+};
+
+exports.conf = {
+  enabled: true, //komutut açtık
+  guildOnly: false, //sadece servere özel yapmadık
+  aliases: ['ping','p'], //farklı çağrılar ekledik
+  permLevel: 0 //kimlerin kullanabileceğini yazdık (bot.js dosyasında perm leveller yazıyor)
+};
+
+exports.help = {
+  name: 'Naber', //adını belirledik (kullanmak için gereken komut)
+  description: 'Bota naber dersin.', //açıklaması
+  usage: 'Naber' //komutun kullanım şekli (mesela hava <bölge>)
+};
+
+//komut taslağımız basit anlatımını zaten dosyalarda bulursunuz
+//bu ise bizim komut taslağımız
+//bunun üzerinden gideceğiz
