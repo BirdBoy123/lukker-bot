7a8bf2952db04ac734915a3fe1000bed70f5c0a9
BirdBoy123 <43483218+BirdBoy123@users.noreply.github.com>
2018-10-16 18:50:19 +0300

    Update reboot.js

--- komutlar/reboot.js
+++ komutlar/reboot.js
@@ -1,24 +1,22 @@
 const Discord = require('discord.js');
 
-
-exports.run = function(client, message) {
-
-    message.channel.send("Bot yeniden başlatılıyor").then(msg => {
-        console.log("[BOT]Yeniden başlatılıyor");
-        process.exit(0);
-    });
-
+exports.run = (client, message, args) => {
+	
+    message.channel.send(`Bot Yeniden Başlatıldı!`).then(msg => {
+    console.log(`Discord: Bot yeniden başlatılıyor...`);
+    process.exit(0);
+  })
 };
 
 exports.conf = {
-  enabled: true, 
-  guildOnly: false, 
+  enabled: true,
+  guildOnly: false,
   aliases: [],
-  permLevel: 3
+  permLevel: 4
 };
 
 exports.help = {
-  name: 'reboot', 
+  name: 'reboot',
   description: 'Botu yeniden başlatır.',
   usage: 'reboot'
 };
